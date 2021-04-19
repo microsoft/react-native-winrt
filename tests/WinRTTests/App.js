f614e6e13d8c1f59412c0cac3b60a03ec2a2f294
UJJWAL CHADHA <ujchadha@microsoft.com>
2021-04-19 11:44:40 -0700

    test ui improvements

--- tests/WinRTTests/App.js
+++ tests/WinRTTests/App.js
@@ -11,6 +11,7 @@ import {
     ScrollView,
     View,
     Text,
+    Button
 } from 'react-native';
 import { Scenario } from './Scenario';
 import { styles } from './Styles';
@@ -25,28 +26,55 @@ import { makeInheritanceTestScenarios } from './InheritanceTests'
 
 class App extends Component {
     test = new TestComponent.Test();
-    state = { completedCount: 0, passCount: 0 };
     completedCount = 0;
     passCount = 0;
     failures = "Failed Scenarios:\r\n";
 
-    scenarios = [
-        ...makePropertiesTestScenarios(this),
-        ...makeBasicFunctionTestScenarios(this),
-        ...makeArrayTestScenarios(this),
-        ...makeDelegateAndEventTestScenarios(this),
-        ...makeAsyncTestScenarios(this),
-        ...makeCollectionsTestScenarios(this),
-        ...makeInheritanceTestScenarios(this),
-    ];
+    testSuites = [
+        {
+            name: "Property Tests",
+            scenarios: makePropertiesTestScenarios(this),
+        }, {
+            name: "Function Tests",
+            scenarios: makeBasicFunctionTestScenarios(this),
+        }, {
+            name: "Array Tests",
+            scenarios: makeArrayTestScenarios(this),
+        }, {
+            name: "Delegate Tests",
+            scenarios: makeDelegateAndEventTestScenarios(this),
+        }, {
+            name: "Async Tests",
+            scenarios: makeAsyncTestScenarios(this),
+        }, {
+            name: "Collections Tests",
+            scenarios: makeCollectionsTestScenarios(this),
+        }, {
+            name: "Inheritance Tests",
+            scenarios: makeInheritanceTestScenarios(this),
+        }
+    ]
+
+    scenarioNameToTestSuiteMap = this.testSuites.reduce((acc, testSuite) => {
+        testSuite.scenarios.forEach(scenario => {
+            acc[scenario.name] = testSuite.name
+        })
+        return acc;
+    }, {});
+
+    state = { 
+        completedCount: 0, 
+        passCount: 0, 
+        testSuiteCompletedCount: this.testSuites.reduce((acc, testSuite) => { return {...acc, [testSuite.name]: 0}}, {}),
+        testSuitePassCount: this.testSuites.reduce((acc, testSuite) => { return {...acc, [testSuite.name]: 0}}, {}),
+        testSuiteVisibility: this.testSuites.reduce((acc, testSuite) => { return {...acc, [testSuite.name]: false}}, {}),
+        filterText: "", 
+    };
+
+
 
     runSync(scenario, fn) {
-        if (scenario.result != TestResult.NotRun) {
-            --this.completedCount;
-            if (scenario.result == TestResult.Pass) --this.passCount;
-            scenario.result = TestResult.NotRun;
-        }
-
+        let previousResult = scenario.result === TestResult.NotRun ? null : scenario.result;
         var result = TestResult.Fail;
         try {
             fn();
@@ -57,54 +85,64 @@ class App extends Component {
 
         scenario.result = result;
         scenario.emit('completed');
-        this.onSingleTestCompleted(scenario);
+        this.onSingleTestCompleted(scenario, previousResult);
     }
 
     runAsync(scenario, fn) {
-        if (scenario.result != TestResult.NotRun) {
-            --this.completedCount;
-            if (scenario.result == TestResult.Pass) --this.passCount;
-            scenario.result = TestResult.NotRun;
-        }
+        let previousResult = scenario.result === TestResult.NotRun ? null : scenario.result;
         var result = TestResult.Fail;
         new Promise(fn).then(() => {
             result = TestResult.Pass;
             scenario.result = result;
             scenario.emit('completed');
-            this.onSingleTestCompleted(scenario);
+            this.onSingleTestCompleted(scenario, previousResult);
         }).catch(e => {
             scenario.failureText = e.message;
             scenario.result = result;
             scenario.emit('completed');
-            this.onSingleTestCompleted(scenario);
+            this.onSingleTestCompleted(scenario, previousResult);
         })
     }
 
-    onSingleTestCompleted(scenario) {
-        ++this.completedCount;
-        if (scenario.result == TestResult.Pass) {
+    onSingleTestCompleted(scenario, previousResult) {
+        const testSuiteName = this.scenarioNameToTestSuiteMap[scenario.name];
+        if (!previousResult) ++this.completedCount;
+        let resultState = {
+            completedCount: this.completedCount,
+            passCount: this.passCount,
+            testSuiteCompletedCount: this.state.testSuiteCompletedCount,
+            testSuitePassCount: this.state.testSuitePassCount,
+        }
+        if (!previousResult) resultState.testSuiteCompletedCount[testSuiteName] = this.state.testSuiteCompletedCount[testSuiteName] + 1;
+
+        if (scenario.result === TestResult.Pass && previousResult !== TestResult.Pass) {
             ++this.passCount;
-        } else {
+            resultState.passCount = this.passCount;
+            resultState.testSuitePassCount[testSuiteName] = this.state.testSuitePassCount[testSuiteName] + 1;
+        } else if (scenario.result === TestResult.Fail && previousResult === TestResult.Pass){
+            --this.passCount;
+            resultState.passCount = this.passCount;
+            resultState.testSuitePassCount[testSuiteName] = this.state.testSuitePassCount[testSuiteName] - 1;
             this.failures += scenario.name + "\r\n";
         }
 
-        this.setState({
-            completedCount: this.completedCount,
-            passCount: this.passCount,
-        });
+        this.setState(resultState);
+    }
+
+    allScenarios() {
+        return this.testSuites.reduce((acc, val) => acc.concat(val.scenarios), []);
     }
 
     inProgress() {
-        return this.state.completedCount != this.scenarios.length;
+        return this.state.completedCount != this.allScenarios().length;
     }
 
     passed() {
-        return this.state.passCount == this.scenarios.length;
+        return this.state.passCount == this.allScenarios().length;
     }
 
     resultText() {
-        if (!this.inProgress() && !this.passed())
-        {
+        if (!this.inProgress() && !this.passed()) {
             TestComponent.Test.logFailures(this.failures);
         }
 
@@ -118,9 +156,18 @@ class App extends Component {
     }
 
     componentDidMount() {
-        for (var scenario of this.scenarios) {
+        for (let scenario of this.allScenarios()) {
             scenario.invoke(scenario);
         }
+    }
+
+    toggleTestSuiteVisibility(testSuite) {
+        this.setState({
+            testSuiteVisibility: { 
+                ...this.state.testSuiteVisibility,
+                [testSuite.name]: !this.state.testSuiteVisibility[testSuite.name]
+            },            
+        })
     }
 
     render() {
@@ -135,8 +182,30 @@ class App extends Component {
                         <Text style={[styles.rowItem, this.textStyle()]}>{this.resultText()}</Text>
                     </View>
                     {
-                        this.scenarios.map((scenario, idx) => (
-                            <Scenario key={scenario.name} scenario={scenario} />
+                        this.testSuites.map((testSuite, idx) => (
+                            <View key={testSuite.name}>
+                                <View style={styles.listEntry}>
+                                    <Text style={[styles.rowItem, {fontWeight: 'bold'}]}>{testSuite.name}</Text>
+                                    <Text style={[styles.rowItem, {flex: 1}]}>{
+                                        this.state.testSuitePassCount[testSuite.name] 
+                                        + "/" 
+                                        + this.state.testSuiteCompletedCount[testSuite.name]
+                                        + " passed. "
+                                        + (testSuite.scenarios.length - this.state.testSuiteCompletedCount[testSuite.name])
+                                        + " pending."
+                                    }</Text>
+                                    <Button style={[styles.rowItem, {flex: 1}]} 
+                                        title={this.state.testSuiteVisibility[testSuite.name] ? "Hide all tests" : "Show all tests"}
+                                        onPress={() => this.toggleTestSuiteVisibility(testSuite)}/>
+                                </View>
+                                {
+                                    testSuite.scenarios.map((scenario, idx) => (
+                                        this.state.testSuiteVisibility[testSuite.name] || scenario.result !== TestResult.Pass
+                                            ? <Scenario key={scenario.name} scenario={scenario} />
+                                            : <View key={idx} style={{marginHorizontal: 1}}/>
+                                    )) 
+                                }
+                            </View>
                         ))
                     }
                 </ScrollView>
