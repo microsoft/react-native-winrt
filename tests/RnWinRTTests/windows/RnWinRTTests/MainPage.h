de82936499f9e2f84bc5c3af9ca3c36c0ef6880e
Duncan Horn <dunhor@microsoft.com>
2021-12-10 14:31:22 -0800

    run clang-format

--- tests/RnWinRTTests/windows/RnWinRTTests/MainPage.h
+++ tests/RnWinRTTests/windows/RnWinRTTests/MainPage.h
@@ -1,7 +1,7 @@
-// Copyright (c) Microsoft Corporation. 
+﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-
 #pragma once
+
 #include "MainPage.g.h"
 #include <winrt/Microsoft.ReactNative.h>
 
