8f675a33b47b29b5cda7e00018b132388a8ede3d
Duncan Horn <dunhor@microsoft.com>
2022-02-09 16:50:56 -0800

    Script updates

--- scripts/cherry-pick-to-release.cmd
+++ scripts/cherry-pick-to-release.cmd
@@ -23,7 +23,17 @@ goto :eof
 :cherrypick_to_release
     set BRANCHNAME=cherrypick_%COMMIT%_to_%1
     git checkout -b %BRANCHNAME% origin/release/%1
+    if !ERRORLEVEL! NEQ 0 (
+        echo ERROR: Failed to create branch '%BRANCHNAME%'. This may be because the branch already exists, or because you have local changes
+        exit /B !ERRORLEVEL!
+    )
+
     git push -u origin %BRANCHNAME%
+    if !ERRORLEVEL! NEQ 0 (
+        echo ERROR: Failed to push branch '%BRANCHNAME%'
+        exit /B !ERRORLEVEL!
+    )
+
     git cherry-pick %COMMIT%
     if !ERRORLEVEL! NEQ 0 (
         echo ERROR: cherry-pick failed. Please check the commit id and try again or perform this process manually
@@ -31,5 +41,10 @@ goto :eof
     )
 
     git push
+    if !ERRORLEVEL! NEQ 0 (
+        echo ERROR: push to server failed
+        exit /B !ERRORLEVEL!
+    )
+
     :: TODO: How to delete these branches locally? Do it at the end after checking out master?
-    :: git branch -d %BRANCHNAME%
+    git branch -d %BRANCHNAME%
