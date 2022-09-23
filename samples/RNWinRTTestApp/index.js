314ace78caf1e74e4b60b87a02117c4e34d931d7
Duncan Horn <dunhor@microsoft.com>
2022-09-22 18:49:17 -0700

    Update to 0.70

--- samples/RNWinRTTestApp/index.js
+++ samples/RNWinRTTestApp/index.js
@@ -9,4 +9,5 @@ import 'react-native-winrt';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
 AppRegistry.registerComponent(appName, () => App);
